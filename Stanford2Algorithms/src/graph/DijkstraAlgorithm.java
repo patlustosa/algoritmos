68e486c676c472d2e802c669affa3867775cc595
patlustosa <patlustosa@gmail.com>
2012-12-17 21:42:17 -0200

    Alguns poucos avancos no Dijkstra com heap

--- Stanford2Algorithms/src/graph/DijkstraAlgorithm.java
+++ Stanford2Algorithms/src/graph/DijkstraAlgorithm.java
@@ -1,5 +1,7 @@
 package graph;
 
+import heap.HeapNode;
+
 import java.util.Collections;
 import java.util.Vector;
 
@@ -96,52 +98,38 @@ public class DijkstraAlgorithm {
 		pathCost = new int[qntNodes];
 		pathCost[from.number] = 0;
 		
+		//initialize heap
+		HeapNode heap = new HeapNode(qntNodes);
+		restoreHeapProperties(from);
+		
 		//main loop
-		while(true){
+		while(true){ //TODO rever essa condicao
 			Edge min = null;
 			int minValue = Integer.MAX_VALUE;
 			
-			//find the edge in frontier with minimum path cost 
-			for(Edge e : edges){
-				if((belongsToX[e.n1.number] && !belongsToX[e.n2.number]) 
-				|| (!belongsToX[e.n1.number] && belongsToX[e.n2.number])){ //one node in X and other node in V-X: node in the frontier
-					//get parent
-					Node parentNode = e.n1;
-					if(!belongsToX[e.n1.number]){
-						parentNode = e.n2;
-					}
-					
-					//calculate the cost of path to e
-					int ePathCost =  e.cost + pathCost[parentNode.number];
-					
-					if(ePathCost < minValue){ // e is better than previous min
-						min = e;
-						minValue = ePathCost;
-					}
-				}
-			}
-			
-			//if didnt find an edge with one node in X and other node in V-X, the search is over.
-			if(min == null){
-				break;
-			}
+			//extractMin from heap: find the edge in frontier with minimum path cost 
+			Node newNode = heap.extractMin();
 			
 			//add new node to X
-			Node newNode = min.n1;
-			if(!belongsToX[min.n2.number]){
-				newNode = min.n2;
-			}
 			belongsToX[newNode.number] = true;
 			
 			//set the cost of newNode path
-			pathCost[newNode.number] = minValue;
+			pathCost[newNode.number] = minValue; //TODO find min value
 			
 			//min is the edge from which i get to newNode. Need to set this so that I can trace the path back when I find the destination
-			pathEdge[newNode.number] = min;
+			pathEdge[newNode.number] = min; //TODO find min edge
+			
+			//restore heap properties
+			restoreHeapProperties(newNode);
 		}
 	}
 	
 	
+	private void restoreHeapProperties(Node newNode) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public Vector<Edge> getEdgesOnPath(Node destination){
 		Node currentNode = destination;
 		
